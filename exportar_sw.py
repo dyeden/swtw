9fe8270596cff597d30150fd0b772c8fea6823bb
Dyeden Monteiro <dyedenm@gmail.com>
2016-08-10 09:18:48 -0300

    atualizando

--- exportar_sw.py
+++ exportar_sw.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import json
 import teamwork
 import unicodecsv
 import codecs
@@ -10,43 +11,13 @@ TW_KEY = 'fdafdafa'
 USER_ID = 84684684
 #
 # instance = teamwork.Teamwork('terras.teamwork.com', TW_KEY)
-teste = """"date","Person","Used time","Description","Item id","Item name","Task id","Task name","Link"
-"1/8/2016","Dyeden Monteiro","4","","1756","T: BigData: elaborar proposta de frontend - Part 3","6344","Inserção do serviço na AWS","https://www.scrumwise.com/scrum/#/time-entry/id-60727-4278-1"
-"1/8/2016","Dyeden Monteiro","3","","1756","T: BigData: elaborar proposta de frontend - Part 3","6345","Pesquisa: Como ler dados raster de forma paralela - Part 1","https://www.scrumwise.com/scrum/#/time-entry/id-60727-4278-5"
-"4/8/2016","Dyeden Monteiro","3","","1756","T: BigData: elaborar proposta de frontend - Part 3","6494","Pesquisa sensores e obtenção dos dados  raster  (Resource-sat, sentinel, modis ...) Part1","https://www.scrumwise.com/scrum/#/time-entry/id-60727-4314-12"
-"4/8/2016","Dyeden Monteiro","4","","1756","T: BigData: elaborar proposta de frontend - Part 3","6493","Pesquisa sensores e obtenção dos dados  raster  (Resource-sat, sentinel, modis ...) Part2","https://www.scrumwise.com/scrum/#/time-entry/id-60727-4314-15"
-"5/8/2016","Dyeden Monteiro","1","","1756","T: BigData: elaborar proposta de frontend - Part 3","6495","Reunião Carlos","https://www.scrumwise.com/scrum/#/time-entry/id-60727-4314-25"
-"""
-
-def read_csv(path_sw_file):
-    # text = open(path_sw_file, "r", )
-    text = codecs.open(path_sw_file, 'rt', encoding='utf-8')
-    # print text
-    f = StringIO(teste)
-    # print f.readlines()
-    reader = unicodecsv.reader(text,delimiter=",", encoding='utf-8')
-    row = reader.next()
-    print row
-    row = reader.next()
-    print row
-    row = reader.next()
-    print row
 
 
-    # with open(path_sw_file) as csvfile:
-    #     # reader = csv.DictReader(text, delimiter=",")
-    #     reader = unicodecsv.reader(text, encoding='utf-8')
-    #     for row in reader:
-    #         print row
-    #         # print(row["Date"], row["Person"])
+def read_json(path_sw_file):
+    text = codecs.open(path_sw_file, 'rt', encoding='utf-8').read()
+    data = json.loads(text, encoding='utf8')
 
 def inserir_hora(project_id, path_sw_file):
-    # project_id: Project ID
-    # date: datetime.date Date of time entry
-    # duration: datetime.timedelta Duration
-    # user_id: Integer Id of person
-    # description: String Id of person
-    # start_time: datetime.timedelta
 
     project_id = 221896
     date = datetime.date(2016,8,9)
@@ -54,6 +25,6 @@ def inserir_hora(project_id, path_sw_file):
     user_id = USER_ID
     description = "testando teamwork"
     start_time = datetime.datetime.now()
-    instance.save_project_time_entry(project_id, date, duration, user_id,description, start_time)
+    # instance.save_project_time_entry(project_id, date, duration, user_id,description, start_time)
 
-read_csv("./dados_sw/Scrumwise Time Log 2016-08-09_teste.csv")
+read_json("/home/dyeden/ProjetosPython/teamwork_api/swtw/dados_sw/data.json")
